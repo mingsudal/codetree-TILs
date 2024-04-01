ae6522026653b55607dda1292c4506016c188def
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-04-01 19:46:54 +0900

    Title: 두 번의 연산, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240401/두 번의 연산/two-operations.py
+++ 240401/두 번의 연산/two-operations.py
@@ -2,10 +2,15 @@ a = int(input())
 b =0
 if a%2 !=0 :
     a+=3
-    if a%3 ==0 :
-        b = a//3
-        print(b)
+    if a%3 ==0:
+        a//=3
+        print(a)
     else:
         print(a)
+
 else:
-    print(a)
+    if a%3 ==0:
+        a//=3
+        print(a)
+    else:
+        print(a)
