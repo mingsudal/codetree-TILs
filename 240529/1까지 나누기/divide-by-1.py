1aa9e7b7d6f7a64e72300e6c1dbe88b54a2f194d
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-05-29 14:16:03 +0900

    Title: 1까지 나누기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240529/1까지 나누기/divide-by-1.py
+++ 240529/1까지 나누기/divide-by-1.py
@@ -1,8 +1,8 @@
-n = int(input())
-count=0
-for i in range(1,n):
-    n/=i
-    count+=1
-    if n <=1:
-        break
-print(count)
+n = int(input())  # 사용자로부터 정수 n 입력 받음
+count = 0  # 나눗셈 횟수를 세는 변수
+
+while n > 1:  # n이 1 이하가 될 때까지 반복
+    n //= count + 1  # n을 (count + 1)로 나눔 (1부터 시작하므로 count에 1을 더한 값을 사용)
+    count += 1  # 나눗셈 횟수를 증가
+
+print(count)  # 나눗셈 횟수를 출력
