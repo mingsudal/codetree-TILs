c3aa5c1f4f339d44b797f4b2159bdaf2d2475b0e
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-07 18:14:08 +0900

    Title: 합성수 판별기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240607/합성수 판별기/factorization-discriminator.py
+++ 240607/합성수 판별기/factorization-discriminator.py
@@ -1,11 +1,15 @@
 n = int(input())
-satisfied = False
-for i in range(2, n):
-    if n % i == 0:
-        is_composite = True
-        break
 
-if is_composite:
-    print("C")
+if n < 2:
+    print("Invalid input")
 else:
-    print("N")
+    is_composite = False
+    for i in range(2, n):
+        if n % i == 0:
+            is_composite = True
+            break
+
+    if is_composite:
+        print("C")
+    else:
+        print("N")
