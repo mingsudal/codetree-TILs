ec21ca4c5d11cc29e0634ddf258d79ef0efed102
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-07 18:35:42 +0900

    Title: ab 사이에 없는 c, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240607/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240607/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,11 +1,6 @@
 a,b,c=map(int,input().split())
 satified = False
-for i in range(a, b + 1):
-    if i % c == 0:
-        satified = True
-        break
-
-if satified:
-    print("YES")
-else:
+if any(i % c == 0 for i in range(a, b + 1)):
     print("NO")
+else:
+    print("YES")
