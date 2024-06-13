2b80f384f68282e3eed61dff918e1c8bd96e3513
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-14 00:21:38 +0900

    Title: 완전수, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240614/완전수/perfect-number.py
+++ 240614/완전수/perfect-number.py
@@ -1,11 +1,15 @@
 start,end=map(int,input().split())
 cnt = 0
-val = 0
-for i in range(start+1, end):
-    val = 0
-    for j in range(1, i):
-        if i % j == 0:
-            val += j
-    if val == i:
+for num in range(start, end + 1):
+    sum_of_divisors = 0
+    
+    # 자신을 제외한 약수의 합을 계산
+    for i in range(1, num):
+        if num % i == 0:
+            sum_of_divisors += i
+    
+    # 완전수인 경우 cnt 증가
+    if sum_of_divisors == num:
         cnt += 1
+
 print(cnt)
