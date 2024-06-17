d140cb341c2455457795313044f2b1e1d60e5fe7
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-17 17:13:59 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240617/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -0,0 +1,22 @@
+numbers = list(map(int, input().split()))
+
+count = 0
+total_sum =0
+found_0 = False
+
+for num in numbers:
+    if num == 0:
+        found_0 = True
+        break
+    if num%2 ==0:
+        total_sum += num
+        count += 1
+
+
+if not found_0:
+    total_sum = sum(numbers)
+    count = len(numbers)
+
+
+# 결과 출력
+print(count,total_sum)
