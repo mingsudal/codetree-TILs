4508b428485a9dac21e96776f9c6cf10bb876d84
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-17 17:24:48 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 121ms, Memory: 72MB, Status: Passed - CodeTree

--- 240617/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
+++ 240617/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -2,21 +2,12 @@ numbers = list(map(int, input().split()))
 
 count = 0
 total_sum = 0
-found_0 = False
 
 for num in numbers:
     if num == 0:
-        found_0 = True
         break
     if num % 2 == 0:
-        total_sum += num
         count += 1
+        total_sum += num
 
-if not found_0:
-    for num in numbers:
-        if num % 2 == 0:
-            total_sum += num
-            count += 1
-
-# 결과 출력
 print(count, total_sum)
