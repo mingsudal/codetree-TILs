9c7525b14aba26ce86c9e99ebaac742fc9f0ab76
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-17 16:23:15 +0900

    Title: 학점 계산기, Time: 118ms, Memory: 73MB, Status: Wrong Answer - CodeTree

---
+++ 240617/학점 계산기/credit-calculator.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(float,input().split()))
+sum =0.0
+average=0
+for i in range(n):
+    sum +=arr[i]
+    average= round(sum/n,1)
+print(average)
+if average >=4.0:
+    print("Perfect")
+elif average>=3.0:
+    print("Good")
+else:
+    print("poor")
