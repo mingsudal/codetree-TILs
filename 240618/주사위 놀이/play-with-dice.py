399c6841f4b1b05aad177c84c1898da5f4a046c7
mingsudal <165634462+mingsudal@users.noreply.github.com>
2024-06-18 17:22:20 +0900

    Title: 주사위 놀이, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240618/주사위 놀이/play-with-dice.py
@@ -0,0 +1,9 @@
+count_arr = [0] * 7
+arr = list(map(int, input().split()))
+for elem in arr:
+    count_arr[elem] += 1
+
+# 개수 출력
+for i in range(1, 7):
+    cnt = count_arr[i]
+    print(f"{i} - {cnt}")
